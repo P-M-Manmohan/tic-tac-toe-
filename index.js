a917127616bb569142d0d1f34f564b19a59c6e7f
P M Manmohan <manu6520042@gmail.com>
2023-05-29 21:20:35 +0530

    added some js (event listeners)

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-const menu = document.querySelector(".menu");
-const menuItems = menu.querySelector(".items");
+const App = {
+  //All selected html elemnts
+  $: {
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]'),
+  },
 
-menu.addEventListener("click", (event) => {
-  menuItems.classList.toggle("hidden");
-});
+  init() {
+    App.registerEventListeners();
+  },
+
+  registerEventListeners() {
+    console.log(App.$.squares);
+
+    //DONE
+    App.$.menu.addEventListener("click", (event) => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
+    //TODO
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset the game");
+    });
+    //TODO
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("new round");
+    });
+    //TODO
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        console.log(`Square with id ${event.target.id} was clicked`);
+      });
+    });
+  },
+};
+
+window.addEventListener("load", App.init);
